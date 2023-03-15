37a97505e2c86e2d1b67a503577b35ef01283dc3
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-03-14 23:43:29 -0300

    html css y js terminado

---
+++ index.js
@@ -0,0 +1,114 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+const PASS_ONE = document.getElementById("passOne")
+const PASS_TWO = document.getElementById("passTwo")
+
+function randomPassword() {
+    let arr = []
+    for (let i = 0; i < characters.length; i++) {
+        if(arr.length < 13) {
+        let randomChar = Math.floor(Math.random()*characters.length)
+        arr.push(characters[randomChar])
+        }
+    }
+    console.log(arr)
+    let passwordGenerated = arr.join('')
+    return passwordGenerated;
+}
+
+function generarPass() {
+    PASS_ONE.innerText = randomPassword()
+    PASS_TWO.innerText = randomPassword()
+}
